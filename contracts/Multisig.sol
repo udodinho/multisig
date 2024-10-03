04b20caabb40aff6a2de23cdbe4a9e22c1b01d23
udodinho <udodinho200@gmail.com>
2024-10-03 07:12:29 +0100

    feat: implement transfer initiation

---
+++ contracts/Multisig.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Multisig {
+    uint256 public quorum;
+    uint8 public noOfValidSigners;
+    uint256 public txCount;
+
+    struct Transaction {
+        uint256 id;
+        uint256 amount;
+        address sender;
+        address recipient;
+        bool isCompleted;
+        uint256 timestamp;
+        uint256 noOfApproval;
+        address tokenAddress;
+        address[] transactionSigners;
+    }
+
+    struct Quorum {
+        uint8 id;
+        uint256  noOfApprovals;
+        address[] quorumSigners;
+        bool isApproved;
+    }
+
+    mapping (uint => Quorum) quorums;
+    mapping(address => bool) isValidSigner;
+    mapping(uint => Transaction) transactions; // txId -> Transaction
+    // signer -> transactionId -> bool (checking if an address has signed)
+    mapping(address => mapping(uint256 => bool)) hasSigned;
+
+    constructor(uint256 _quorum, address[] memory _validSigners) {
+        require(_validSigners.length > 1, "few valid signers");
+        require(_quorum > 1, "quorum is too small");
+
+
+        for(uint256 i = 0; i < _validSigners.length; i++) {
+            require(_validSigners[i] != address(0), "zero address not allowed");
+            require(!isValidSigner[_validSigners[i]], "signer already exist");
+
+            isValidSigner[_validSigners[i]] = true;
+        }
+
+        noOfValidSigners = uint8(_validSigners.length);
+
+        if (!isValidSigner[msg.sender]){
+            isValidSigner[msg.sender] = true;
+            noOfValidSigners += 1;
+        }
+
+        require(_quorum <= noOfValidSigners, "quorum greater than valid signers");
+        quorum = _quorum;
+    }
+
+    function transfer(uint256 _amount, address _recipient, address _tokenAddress) external {
+        require(msg.sender != address(0), "address zero found");
+        require(isValidSigner[msg.sender], "invalid signer");
+
+        require(_amount > 0, "can't send zero amount");
+        require(_recipient != address(0), "address zero found");
+        require(_tokenAddress != address(0), "address zero found");
+
+        require(IERC20(_tokenAddress).balanceOf(address(this)) >= _amount, "insufficient funds");
+
+        uint256 _txId = txCount + 1;
+        Transaction storage trx = transactions[_txId];
+        
+        trx.id = _txId;
+        trx.amount = _amount;
+        trx.recipient = _recipient;
+        trx.sender = msg.sender;
+        trx.timestamp = block.timestamp;
+        trx.tokenAddress = _tokenAddress;
+        trx.noOfApproval += 1;
+        trx.transactionSigners.push(msg.sender);
+        hasSigned[msg.sender][_txId] = true;
+
+        txCount += 1;
+    }
+}
